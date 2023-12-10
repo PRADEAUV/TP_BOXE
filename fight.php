ff7bcb8d84056827036a93a19ae59f525b1c61a3
Virginie PRADEAU <pradeauv@hotmail.com>
2023-12-10 23:18:02 +0100

    fonction boucle Round

--- fight.php
+++ fight.php
@@ -1,5 +1,5 @@
 <?php
-
+// version 2 avec une fonction pour les boucles
 require_once ('Ippo.php');
 require_once ('challenger.php');
 
@@ -21,79 +21,11 @@ public function fighting($boxer1,$boxer2)
     //celui avec la vitesse la plus élevée frappe en premier
 if ($boxer1->speed> $boxer2->speed) //boxer1 est plus rapide
 {
-    for ($i=1; $i <10; $i++) //boucle des rounds
-{ 
-    if (($boxer1->stamina<=0) || ($boxer2->stamina<=0) ) 
-    {
-        $this->fincombatAvtDixRound($boxer1,$boxer2);
-     }
-   else
-   {
-    echo"round $i \n";
-    // probabilité d' un coup critique
-    $this->probKO($boxer1,$boxer2);
-    // baisse de la stamina
-    $boxer2->stamina=$boxer2->stamina-$boxer1->strength;
-    echo " La stamina de $boxer1->name est $boxer1->stamina \n";
-    echo " La stamina de $boxer2->name est $boxer2->stamina \n";
-    // les valeurs de la force sont remises à leur valeurs initiales
-    $boxer1->strength=$tempstrength1;
-    $boxer2->strength=$tempstrength2;
-    $i++;
-    if (($boxer1->stamina<=0) || ($boxer2->stamina<=0) ) 
-        {
-            $this->fincombatAvtDixRound($boxer1,$boxer2);
-         }
-    echo'round'. $i ." \n";
-    // probabilité d'un coup critique
-    $this->probKO($boxer1,$boxer2);
-    // Baisse de la stamina
-    $boxer1->stamina=$boxer1->stamina-$boxer2->strength;
-    echo " La stamina de $boxer1->name est $boxer1->stamina \n";
-    echo " La stamina de $boxer2->name est $boxer2->stamina \n";
-    // les valeurs de la force sont remises à leur valeurs initiales
-   $boxer1->strength=$tempstrength1;
-   $boxer2->strength=$tempstrength2;
-    }
-}
+    $this->boucleRounds($boxer1,$boxer2);
 }
 elseif ($boxer2->speed> $boxer1->speed) // boxer2 est plus rapide
 {
-    for ($i=1; $i <10; $i++) //boucle des rounds
-    { 
-        if (($boxer1->stamina<=0) || ($boxer2->stamina<=0) ) 
-        {
-            $this->fincombatAvtDixRound($boxer1,$boxer2);
-         }
-        else
-        {
-        echo"round $i \n";
-        // Probabilité du coup critique
-        $this->probKO($boxer1,$boxer2);
-        //baissse de la stamina
-        $boxer1->stamina=$boxer1->stamina-$boxer2->strength;
-        echo " La stamina de $boxer1->name est $boxer1->stamina \n";
-        echo " La stamina de $boxer2->name est $boxer2->stamina \n";
-        // les valeurs de la force sont remises à leur valeurs initiales
-        $boxer1->strength=$tempstrength1;
-        $boxer2->strength=$tempstrength2;
-        $i++;
-        if (($boxer1->stamina<=0) || ($boxer2->stamina<=0) ) 
-        {
-            $this->fincombatAvtDixRound($boxer1,$boxer2);
-         }
-        echo'round '. $i ."\n";
-        // probabilité du coup critique
-        $this->probKO($boxer1,$boxer2);
-        // baisse de la stamina
-        $boxer2->stamina=$boxer2->stamina-$boxer1->strength;
-        echo " La stamina de $boxer1->name est $boxer1->stamina \n";
-        echo " La stamina de $boxer2->name est $boxer2->stamina \n";
-        // les valeurs de la force sont remises à leur valeurs initiales
-        $boxer1->strength=$tempstrength1;
-        $boxer2->strength=$tempstrength2;
-        }
-}
+    $this->boucleRounds($boxer2,$boxer1);
 }
 // annonce du vainqueur aprdè les 10 rounds
 echo  ($boxer1->stamina>$boxer2->stamina)? $boxer1->name. " est le vainqueur!!!":$boxer2->name. " est le vainqueur!!!";
@@ -116,14 +48,50 @@ Public function probKO($boxer1,$boxer2)
             echo ' La force est '.$boxer1->strength. '  ' .$boxer2->strength." \n"; 
         }
 }
+//********************************************** */
+public function boucleRounds($arg1,$arg2)
+{
+    $this->tempstrength1=$arg1->strength;
+    $this->tempstrength2=$arg2->strength;
+    for ($i=1; $i <10; $i++) //boucle des rounds
+{ 
+    if (($arg1->stamina<=0) || ($arg2->stamina<=0) ) 
+    {
+        $this->fincombatAvtDixRound($arg1,$arg2);
+     }
+   else
+   {
+    echo"round $i \n";
+    // probabilité d' un coup critique
+    $this->probKO($arg1,$arg2);
+    // baisse de la stamina
+    $arg2->stamina=$arg2->stamina-$arg1->strength;
+    echo " La stamina de $arg1->name est $arg1->stamina \n";
+    echo " La stamina de $arg2->name est $arg2->stamina \n";
+    // les valeurs de la force sont remises à leur valeurs initiales
+    $arg1->strength=$this->tempstrength1;
+    $arg2->strength=$this->tempstrength2;
+    $i++;
+    if (($arg1->stamina<=0) || ($arg2->stamina<=0) ) 
+        {
+            $this->fincombatAvtDixRound($arg1,$arg2);
+         }
+    echo'round'. $i ." \n";
+    // probabilité d'un coup critique
+    $this->probKO($arg1,$arg2);
+    // Baisse de la stamina
+    $arg1->stamina=$arg1->stamina-$arg2->strength;
+    echo " La stamina de $arg1->name est $arg1->stamina \n";
+    echo " La stamina de $arg2->name est $arg2->stamina \n";
+    // les valeurs de la force sont remises à leur valeurs initiales
+    $arg1->strength=$this->tempstrength1;
+    $arg2->strength=$this->tempstrength2;
+    }
+}
+}
 }
 
 
 $fight=new Fight;
 $fight->fighting($boxer1,$boxer2);
-
-
-
-
-
 ?>
